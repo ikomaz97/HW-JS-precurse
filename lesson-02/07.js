b37feabfaa7166a21e96104e787033baa7807edd
ikomaz97 <ikomaz97@gmail.com>
2024-01-25 21:53:34 +0400

    02-07

---
+++ lesson-02/07.js
@@ -0,0 +1,23 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried2 = {...passportMarried, married: true, address: {...passportMarried.address}}
+
+passportMarried2.married = {
+    name: "Petr",
+    surname: "Petrov",
+    married: true,
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+console.log(passportMarried);
+console.log(passportMarried2);
