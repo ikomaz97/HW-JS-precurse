877548367f3bacf7bb68cbba8cd8536bda7c4944
ikomaz97 <ikomaz97@gmail.com>
2024-01-25 21:59:31 +0400

    02-07

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -9,15 +9,7 @@ let passportMarried = {
 
 let passportMarried2 = {...passportMarried, married: true, address: {...passportMarried.address}}
 
-passportMarried2.married = {
-    name: "Petr",
-    surname: "Petrov",
-    married: true,
-    address: {
-        country: "USA",
-        city: "Bobryisk"
-    }
-};
+
 
 console.log(passportMarried);
 console.log(passportMarried2);
