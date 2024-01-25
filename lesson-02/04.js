da6fe13bf0db41e03db66aaed520525065a58a36
ikomaz97 <ikomaz97@gmail.com>
2024-01-25 21:06:20 +0400

    02-04

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,9 +1,9 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
-    if (value1 === value2 && value1 <= maximum ) {
+    if (value1 === value2 && value1 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
     }
 }
 
-giveMeRightCondition(100, 100, 200)
+giveMeRightCondition(100, 100, 200);
