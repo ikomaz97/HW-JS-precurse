7b033cfd8fef2d55d79455b895f5007e5c53754e
ikomaz97 <ikomaz97@gmail.com>
2024-01-25 22:02:56 +0400

    02-07

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,7 +7,7 @@ let passportMarried = {
     }
 };
 
-let passportMarried2 = {...passportMarried, married: true, address: {...passportMarried.address}}
+let passportMarried2 = {...passportMarried, married: true}
 
 
 
