3b08f45d8b389ab124eab9c37133a5c70109ac05
ikomaz97 <ikomaz97@gmail.com>
2024-01-25 18:16:50 +0400

    02-03

---
+++ lesson-02/03.js
@@ -0,0 +1,8 @@
+const giveMeRightNumbers = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+giveMeRightNumbers(-100, 100, -200)
