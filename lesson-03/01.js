95e8e246a8ff3381f3584cb7bdcfc89c560436fa
ikomaz97 <ikomaz97@gmail.com>
2024-01-28 16:19:38 +0400

    03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,5 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+function createColorString() {
+    return colors.filter(el=> [ "черный", "красный", "желтый"].includes(el)).join('-')
+}
+console.log(createColorString());
